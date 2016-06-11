1112f006c7fcf18d6df0c6406eaaaa1410f1817e
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-06-11 18:01:30 +0900

    Added Bookmark schema

--- imports/api/bookmarks/bookmarks.js
+++ imports/api/bookmarks/bookmarks.js
@@ -1,5 +1,52 @@
 import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { _ } from 'meteor/underscore';
 
-const Bookmarks = new Mongo.Collection('bookmarks');
+class BookmarksCollection extends Mongo.Collection {
+  insert(doc, callback) {
+    const ourDoc = doc;
+
+    ourDoc.createdAt = ourDoc.createdAt || new Date();
+    ourDoc.updatedAt = ourDoc.updatedAt || ourDoc.createdAt;
+
+    const result = super.insert(ourDoc, callback);
+    return result;
+  }
+
+  update(selector, modifier) {
+    const exModifier = modifier;
+    if (_.isObject(exModifier) && exModifier.$set) {
+      exModifier.$set.updatedAt = new Date();
+    }
+    const result = super.update(selector, exModifier);
+    return result;
+  }
+}
+
+const Bookmarks = new BookmarksCollection('bookmarks');
+
+Bookmarks.schema = new SimpleSchema({
+  title: {
+    type: String,
+    defaultValue: '',
+  },
+  uri: {
+    type: String,
+  },
+  note: {
+    type: String,
+    optional: true,
+    max: 10000,
+  },
+  createdAt: {
+    type: Date,
+    denyUpdate: true,
+  },
+  updatedAt: {
+    type: Date,
+  }
+});
+
+Bookmarks.attachSchema(Bookmarks.schema);
 
 export default Bookmarks;
